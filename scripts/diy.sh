d35116f0d9076e9071c98f228895d1002acef180
152952849 <152952849@qq.com>
2024-08-17 16:49:39 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,5 +2,5 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
+#添加package
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
