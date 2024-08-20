ab5ac7ad71b4cfda43dfa2aefcce8d27372f084f
152952849 <152952849@qq.com>
2024-08-20 09:48:22 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,4 +2,4 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git oppackage https://github.com/152952849/op_package' >>feeds.conf.default
